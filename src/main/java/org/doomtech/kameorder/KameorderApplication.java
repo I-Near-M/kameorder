753bdb19ad0c7ccaba268736649139f73eaaa46b
near <mguillermolaura@gmail.com>
2025-05-14 22:59:50 -0500

    creacion de proyecto springboot

---
+++ src/main/java/org/doomtech/kameorder/KameorderApplication.java
@@ -0,0 +1,13 @@
+package org.doomtech.kameorder;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class KameorderApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(KameorderApplication.class, args);
+    }
+
+}
